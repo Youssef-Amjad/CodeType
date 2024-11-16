f3df2304ba1f64047abdf4585dac59a13d8221a5
Youssef Amjad <zy1yogi@gmail.com>
2024-11-16 18:53:58 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    build: {
-        outDir: "public",
+    server: {
+        https: true,
     },
     plugins: [
         laravel({
