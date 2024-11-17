19dff73a05b9320f47267e129b26a0bbf9b6a162
Youssef Amjad <zy1yogi@gmail.com>
2024-11-17 22:22:44 +0200

    add google

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -61,10 +61,28 @@
     {{-- Theme --}}
     <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0E1422" />
     <meta name="theme-color" media="(prefers-color-scheme: light)" content="#E4E9F7" />
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5J2RVBDW');
+    </script>
 </head>
 
 <body class="antialiased bg-body-light dark:bg-body-dark dark:text-white">
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5J2RVBDW" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     {{ $slot }}
 
     @include('layouts.loader')
